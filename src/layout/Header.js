325b170e2594e1c4a8142569af763e2d64a0d370
AbduraxmonBozorov <webrazrabochik@gmail.com>
2023-03-31 16:10:04 +0500

    changed

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,9 +4,8 @@ export default function Header(){
             <div className="nav-wrapper container">
             <a href="index.html" className="brand-logo">React Kino</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="sass.html">Sass</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">JavaScript</a></li>
+                
+                
             </ul>
             </div>
         </nav>
